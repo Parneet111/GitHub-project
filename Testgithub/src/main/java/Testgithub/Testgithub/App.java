39b43cb17def6b413d48be901433758692c7a7d4
Gnet Webs 9 <Gnet Webs 9@192.168.0.103>
2019-04-27 13:05:01 +0530

    Test...

---
+++ Testgithub/src/main/java/Testgithub/Testgithub/App.java
@@ -0,0 +1,13 @@
+package Testgithub.Testgithub;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello hi!" );
+    }
+}
